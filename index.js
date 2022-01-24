5881224193b0c25574b721a5567da3bd8a3603a1
pinvisingh <61360147+pinvisingh@users.noreply.github.com>
2022-01-24 16:57:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+// setInterval(alarmclock , 10);
+//             function alarmclock() {
+//                 var time = new Date();
+//                 var hrs= time.getHours();
+//                 var min = time.getMinutes();
+//                 var sec = time.getSeconds();
+//                 var en = 'AM';
+ 
+
+//                 if(hrs>12){
+//                     en = 'PM';
+//                 }
+//                 if(hrs>12){
+//                     hrs = hrs-12;
+//                 }
+//                 if(hrs==0){
+//                     hrs =12;
+//                 }
+//                 if(hrs<10){
+//                     hrs = '0' + hrs ; 
+//                 }
+//                 if(min<10){
+//                     min = '0' + min ; 
+//                 }
+//                 if(sec<10){
+//                     sec = '0' + sec
+//                 }
+
+//                 document.getElementById('clock').innerHTML = hrs + ':' + min + ':' + sec + ' ' + en ; 
+//             }
+
 const currentTime = document.querySelector("#current-time");
 const setHours = document.querySelector("#hours");
 const setMinutes = document.querySelector("#minutes");
